9e30999e5c7168777f3ff7736efbc233fe214dfd
indahnurhayati <80864665+indahnurhayati@users.noreply.github.com>
2021-04-22 09:44:14 +0700

    Add files via upload

---
+++ personUTS/PersonBeraksi.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personUTS;
+
+public class PersonBeraksi {
+    public static void main(String[] args){
+        PersonSebelum personIndah = new PersonSebelum();
+        
+        personIndah.PersonJoe("Joe Smith", 24);
+        personIndah.PersonMary("Mary Sharp", 53);
+    }
+}
---
+++ personUTS/PersonSebelum.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personUTS;
+
+public class PersonSebelum {
+    String name;
+    int age;
+    
+ public void PersonJoe(String name, int age){
+    System.out.println("Nama saya :"+ name);
+    System.out.println("Umur saya :" + age);
+    }
+ public void PersonMary(String name, int age){
+    System.out.println("Nama saya :"+ name);
+    System.out.println("Umur saya :" + age);
+    }
+}
